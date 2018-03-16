4d62b3e4e6fbf751f7b4d7b6c7c2aefc8f737a33
vinny-paris <vinny@iastate.edu>
2018-03-16 17:34:52 -0400

    Data visualization and tabling

---
+++ R/exploratory_analysis.R
@@ -0,0 +1,32 @@
+data <- read.csv('./data/mortality_data.csv', header = T)[,-1]
+head(data)
+
+library(ggplot2)
+
+ggplot(data, aes(years)) + geom_histogram(aes(fill = service)) + facet_grid(~service)
+
+tab <- table(data[,1:2])
+totals <- apply(tab, 1, sum)
+survived <- tab[,11]
+
+#Percent survived
+survived/totals
+
+ratios <- NULL
+holding <- NULL
+for(j in 1:3){
+ for(i in 1:9){
+  holding[i] <- tab[j,i+1] / tab[j,i]
+ }
+ ratios <- cbind(ratios, holding)
+}
+
+colnames(ratios) <- c("exp", "rest", "salloon")
+rownames(ratios) <- c("2/1", "3/2", "4/3", "5/4", "6/5", "7/6", "8/7", "9/8", "10/9")
+
+
+
+
+
+
+x <- fa <- 2 <- d <- 3
